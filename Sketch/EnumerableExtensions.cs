987b5884923929bd5610a60da801f1c8bc171c51
iivchenko <ivan.ivchenko@software.dell.com>
2016-02-28 16:27:22 +0000

    Add EnumerableExtensions

---
+++ Sketch/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2016, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System.Collections.Generic;
+
+namespace Sketch
+{
+    public static class EnumerableExtensions
+    {
+        public static void AddTo<T>(this IEnumerable<T> source, ICollection<T> target)
+        {
+            foreach (var element in source)
+            {
+                target.Add(element);
+            }
+        }
+    }
+}
